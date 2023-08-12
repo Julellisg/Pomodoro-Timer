9fa55cb53ae347749d6ac91cd823b9a9e4cc7a5e
julellisg <julianellisg@gmail.com>
2023-08-11 20:39:26 -0400

    Redid time logic, and made timer 100% functional
    - Time for styling next

--- script.js
+++ script.js
@@ -1,100 +1,144 @@
+// Timer div
 const sessionDisplay = document.getElementById('session');
 const timeDisplay = document.getElementById('time');
+// Controls div
 const startButton = document.getElementById('start');
 const resetButton = document.getElementById('reset');
+// Settings div
 const workTimeInput = document.getElementById('workTime');
 const restTimeInput = document.getElementById('restTime');
+const setCountInput = document.getElementById('setCount');
 
-// var workTime = 2;     // minutes
-// var wTime = workTime * 60;    // seconds
-var wTime = 10; // seconds
+// No values
+var w = 0;  // MINUTES
+var workTime = 0; // MINUTES
+var r = 0;  // MINUTES
+var restTime = 0; // MINUTES
+var time = 0;   // SECONDS
 
-// var restTime = 1; // minutes
-// var rTime = restTime *  60;
-var rTime = 10; // minutes
+var s = 0;
+var set = 0;
 
-const speed = 100;
-let isRunning = false;
-let isWorking = true;   // by default
-let time = wTime;
-let x;
+const speed = 10;
+var paused = true;
+var isWorking = true;
 
-// * Timer Functions
+let intervalId;
+
+// Takes time in SECONDS
 function startTimer() {
+    if (time > 0) {
+        paused = !paused; // Toggle the paused state
 
-    isRunning = !isRunning;
+        if (!paused) {
+            intervalId = setInterval(function () {
+                time--;
+                updateTimerDisplay(time);
 
-    if (isRunning) {
-        console.log("test");
-        x = setInterval(function () {
-            time--;
+                // ! ALTERNATE BETWEEN WORKING AND RESTING
+                if (time <= 0) {
+                    paused = true;
+                    clearInterval(intervalId); // Clear the interval when time reaches 0
+                    // ! Switching to RESTING
+                    if(isWorking) {
+                        isWorking = false;
+                        sessionDisplay.innerHTML = "Resting";
+                        time = restTime;
+                        startTimer();
+                    } 
+                    // ! Switching to WORKING
+                    else {
+                        isWorking = true;
+                        sessionDisplay.innerHTML = "Working";
+                        time = workTime;
+                        startTimer();
 
-            if (time >= 0) {
-                initTimer(time);
-            } else {
-                if (isWorking) {
-                    // Switch to rest time
-                    console.log("Switching to rest time");
-                    isWorking = false;
-                    time = rTime;
-                    sessionDisplay.innerHTML = "Resting";
-                    initTimer(time);
-                } else {
-                    // Switch to work time
-                    console.log("Switching to work time");
-                    isWorking = true;
-                    time = wTime;
-                    sessionDisplay.innerHTML = "Working";
-                    initTimer(time);
+                        // increment sets
+                        if (set > 0) {
+                            s++;
+                            if(s == set) {
+                                clearInterval(intervalId); // Stop the interval after the specified number of sets
+                                s = 0;
+                                sessionDisplay.innerHTML = "Done!"
+                                startButton.disabled = true; // Disable the button during countdown
+                                setTimeout(resetTimer, 3500);
+                            }
+                        } else {
+                            startTimer(); // Start the timer for the next set
+                        }
+                    }
                 }
+            }, speed); // Change interval to 1000 milliseconds (1 second)
+        } else {
+            clearInterval(intervalId);
+        }
 
-                // Check if certain number of loops have occured, then
-                // clearInterval(x);
-            }
-        }, speed);
-    }
-    else {
-        console.log("pause");
-        startButton.innerText = "Resume";
-        clearInterval(x);
-        isWorking = false;
+        // Toggle button text between "Resume" and "Pause"
+        startButton.textContent = paused ? "Resume" : "Pause";
     }
 }
 
-function resetTimer()  {
+// ! MAKE SURE LABELS ARE RESET AS WELL
+function resetTimer() {
+    clearInterval(intervalId);
+    paused = true;
+    isWorking = true;
+    s = 0;
     sessionDisplay.innerHTML = "Working";
-    startButton.innerText = "Start";
-
-    clearInterval(x);
-    isWorking = false;
-    initTimer(wTime);
+    time = workTime;
+    updateTimerDisplay(time);
+    startButton.disabled = false; // Disable the button during countdown
+    startButton.textContent = "Start";
 }
 
-// Displaying the timer dynamically
-function initTimer(t) {
-    const hours = Math.floor((t / 60) / 60);
-    const minutes = Math.floor((t / 60) % 60);
-    const seconds = t % 60;
+function updateTimerDisplay(time) {
+    const hours = Math.floor((time / 60) / 60);
+    const minutes = Math.floor((time / 60) % 60);
+    const seconds = time % 60;
 
     const hh = hours == 0 ? '' : hours + ":";
     const mm = minutes < 10 ? "0" + minutes + ":" : minutes + ":";
-    const ss = seconds < 10 ? "0"+seconds : seconds;
+    const ss = seconds < 10 ? "0" + seconds : seconds;
 
     timeDisplay.innerHTML = hh + mm + ss;
 }
 
 // * Inputs
 workTimeInput.addEventListener('blur', function () {
-    workTime = parseInt(workTimeInput.value, 10);
-    initTimer();
+    w = parseInt(workTimeInput.value, 10);
+    updateTimerDisplay(workTime);
 });
 
 workTimeInput.addEventListener('keydown', function (event) {
     if (event.key === 'Enter') {
-        workTime = parseInt(workTimeInput.value, 10);
+        w = parseInt(workTimeInput.value, 10)  *  60;
         workTimeInput.blur();
-        initTimer();
+        updateTimerDisplay(workTime);
     }
 });
 
-initTimer(wTime);
+restTimeInput.addEventListener('blur', function () {
+    r = parseInt(restTimeInput.value, 10)
+    updateTimerDisplay(restTime);
+});
+
+restTimeInput.addEventListener('keydown', function (event) {
+    if (event.key === 'Enter') {
+        r = parseInt(restTimeInput.value, 10);
+        restTimeInput.blur();
+        updateTimerDisplay(restTime);
+    }
+});
+
+function init() {
+    // Set up the timer for when the user first loads up the page:
+    w = parseInt(workTimeInput.value, 10);
+    r = parseInt(restTimeInput.value, 10);
+    set = parseInt(setCountInput.value, 10);
+    workTime = w * 60;
+    restTime = r * 60;
+    time = workTime;
+    updateTimerDisplay(time);
+}
+
+init();
